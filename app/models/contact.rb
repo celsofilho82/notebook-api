bb32c535b6d436fe2db959e404e62317d50e2b03
Celso Ribeiro <celsofilho82@gmail.com>
2020-03-27 18:01:22 -0300

    use render json with associations

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,13 +1,22 @@
 class Contact < ApplicationRecord
   belongs_to :kind
-  
+
   def author
     "Celso Ribeiro"
   end
   
+  def kind_description
+    self.kind.description
+  end
+  
+
   # Redefinindo o método as_json que é utilizado por debaixo dos panos pelo [render :json] no controller
   def as_json(options={})
-    super(root: true, except: [:created_at, :updated_at])
+    super(
+      except: [:created_at, :updated_at],
+      methods: :kind_description, # Chamando o método que vai gerar no json a descrição do tipo
+      include: {kind: { only: :description }}, # Fazendo a associação com o model kind
+    )
   end
   
 end
