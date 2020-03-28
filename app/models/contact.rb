ced3cb04cb8c937bf02e347a2b188119d7d87112
Celso Ribeiro <celsofilho82@gmail.com>
2020-03-28 16:01:14 -0300

    i18n locale for birthdate tranformation pt-br

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,23 +2,14 @@ class Contact < ApplicationRecord
   
   belongs_to :kind, optional: true 
   has_many :phones, dependent: :destroy
-
-  def author
-    "Celso Ribeiro"
-  end
   
-  def kind_description
-    self.kind.description
-  end
-  
-
   # Redefinindo o método as_json que é utilizado por debaixo dos panos pelo [render :json] no controller
   def as_json(options={})
-    super(
+    contact = super(
       except: [:created_at, :updated_at],
-      methods: :kind_description, # Chamando o método que vai gerar no json a descrição do tipo
-      include: {kind: { only: :description }}, # Fazendo a associação com o model kind
-    )
+      include: {kind: { only: :description }}) # Fazendo a associação com o model kind
+    contact[:birthdate] = I18n.l(self.birthdate) unless self.birthdate.blank? # Alterando a data para pt-br
+    contact
   end
   
 end
