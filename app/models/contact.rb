1971787f42f515ef63f69cdb54cf548085493e54
Celso Ribeiro <celsofilho82@gmail.com>
2020-03-28 16:35:49 -0300

    permit nested attribute for contact phones create

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,12 +2,14 @@ class Contact < ApplicationRecord
   
   belongs_to :kind, optional: true 
   has_many :phones, dependent: :destroy
+  accepts_nested_attributes_for :phones
   
   # Redefinindo o método as_json que é utilizado por debaixo dos panos pelo [render :json] no controller
   def as_json(options={})
     contact = super(
       except: [:created_at, :updated_at],
-      include: {kind: { only: :description }}) # Fazendo a associação com o model kind
+      include: {kind: { only: :description }, phones: { only: :number }} # Fazendo a associação com o model kind
+      ) 
     contact[:birthdate] = I18n.l(self.birthdate) unless self.birthdate.blank? # Alterando a data para pt-br
     contact
   end
