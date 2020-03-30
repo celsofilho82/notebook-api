bcb7c81ba77c09fbf9daf7de125de1c8272acb29
Celso Ribeiro <celsofilho82@gmail.com>
2020-03-30 17:08:22 -0300

    setting associations for model contact

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,6 +1,9 @@
 class ContactSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :birthdate
-
+  belongs_to :kind
+  has_one :address
+  has_many :phones
+  
   def attributes(*args)
     contact = super(*args)
     # pt-BR --> contact[:birthdate] = I18n.l(object.birthdate) unless object.birthdate.blank?
