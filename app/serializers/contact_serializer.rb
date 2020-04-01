2bb7aaf791a9ea39fbe02521dc3ca858442f6c2e
Celso Ribeiro <celsofilho82@gmail.com>
2020-04-01 17:04:41 -0300

    add links HATEOAS in response JSON

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,8 +1,20 @@
 class ContactSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :birthdate #:author
-  belongs_to :kind
   has_one :address
   has_many :phones
+  belongs_to :kind do
+    link(:kind) { kind_url(object.kind.id) } 
+  end
+    
+
+  # Configurando a API para trabalhar com a especifiação JSONAPI HATEOAS
+  # Esse configuração mostra o link para o documento que vai conter a json response
+  # link(:self) { contact_path(object.id) }
+  # link(:kind) { kind_path(object.kind.id) }
+
+  # Código alterado para informar a url completa do recurso
+  link(:self) { contact_url(object.id) }
+  link(:kind) { kind_url(object.kind.id) }
 
   # Informações extras no json, "atributo virtual"
   # def author
