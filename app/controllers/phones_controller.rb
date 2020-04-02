349d648c789974290af8db1e87382ada4dfd31ac
Celso Ribeiro <celsofilho82@gmail.com>
2020-04-02 19:02:02 -0300

    crud with active model serializer on has_many phones

--- app/controllers/phones_controller.rb
+++ app/controllers/phones_controller.rb
@@ -1,24 +1,48 @@
 class PhonesController < ApplicationController
-  before_action :set_phones, only: [:show]
+  before_action :set_contact
 
-   # GET /phones/1
-  def show
-    render json: @phones
+  # DELETE /contact/1/phones
+  def destroy
+    phone = Phone.find(phone_params[:id])
+    phone.destroy
   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_phones
-      if params[:contact_id]
-        @phones = Contact.find(params[:contact_id]).phones
-        return @phones
-      end
-      @phones = phones.where(id: params[:id])
+  # POST /contact/1/phone
+  def create
+    @contact.phones << Phone.new(phone_params)
+
+    if @contact.save
+      render json: @contact.phones, status: :created
+    else
+      render json: @contact.errors, status: :unprocessable_entity
     end
 
-    # Only allow a trusted parameter "white list" through.
-    def phones_params
-      params.require(:phones).permit(:id, :number)
-    end
+  end
+
+  # PATCH /contact/1/phones
+  def update
+    phone = Phone.find(phone_params[:id])
+    if phone.update(phone_params)
+      render json: @contact.phones    
+    else
+      render json: @contact.errors, status: :unprocessable_entity
+    end 
+  end
+ 
+  def show
+   render json: @contact.phones
+  end
+
+ private
+   # Use callbacks to share common setup or constraints between actions.
+   def set_contact
+    @contact = Contact.find(params[:contact_id])
+   end
+
+   def phone_params
+    # Usando o active model serializer para deserializar o json recebido nas requisições
+    # create e update
+     ActiveModelSerializers::Deserialization.jsonapi_parse(params)
+   end
 
 end
